63e0797ca356a13657e18b91e4ec1a1947d382c1
Arabinda2004 <arabindabhowmick5604@gmail.com>
2026-09-12 19:09:28 +0530

    Fix file paths in Vercel configuration and implement API entry point

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const app = require("../app");
+const connectDB = require("../config/db");
+
+module.exports = async (req, res) => {
+    await connectDB();
+    return app(req, res);
+};
